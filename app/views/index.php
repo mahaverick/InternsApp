32397cb6d8c4d4303e4ba7ce6173a1d6b45e59d8
Abhijeet Mahavarkar <abhi.mahavarkar@gmail.com>
2014-09-19 19:41:42 +0530

    first commit with config

---
+++ app/views/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>INTERNSHALL APP</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <p>Hello world! This is Abhijeet Mahavarkar.</p>
+
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
