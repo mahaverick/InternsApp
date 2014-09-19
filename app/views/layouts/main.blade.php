f5f642a768be2123e9122a6f546c95bfcfd90347
Abhijeet Mahavarkar <abhi.mahavarkar@gmail.com>
2014-09-19 21:08:36 +0530

    day 1

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -1,4 +1,18 @@
-<html>
+<!DOCTYPE html>
+<html class="no-js" lang="">    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>INTERNSHAL APP</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,700">
+    </head>
     <body>
         @section('sidebar')
             This is the master sidebar.
@@ -7,5 +21,7 @@
         <div class="container">
             @yield('content')
         </div>
+
+        <script src="js/main.js"></script>
     </body>
 </html>
